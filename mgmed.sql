ad0efc84ce305e9baef5fa30ab3abc60775b1c42
FpiehrDoll <mj.grembski@gmail.com>
2022-03-29 12:47:48 +0200

    ahow appointments for staff

--- mgmed.sql
+++ mgmed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 29 Mar 2022, 11:38
+-- Czas generowania: 29 Mar 2022, 12:46
 -- Wersja serwera: 10.4.22-MariaDB
 -- Wersja PHP: 8.0.15
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `mgmed`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `staff_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `staff_id`, `date`) VALUES
+(1, 1, '2022-03-30 10:19:31'),
+(2, 3, '2022-03-31 10:21:50'),
+(3, 2, '2022-03-30 10:22:21');
 
 -- --------------------------------------------------------
 
@@ -47,6 +68,13 @@ INSERT INTO `staff` (`id`, `firstName`, `lastName`) VALUES
 --
 
 --
+-- Indeksy dla tabeli `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
 -- Indeksy dla tabeli `staff`
 --
 ALTER TABLE `staff`
@@ -57,10 +85,26 @@ ALTER TABLE `staff`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT dla tabeli `staff`
 --
 ALTER TABLE `staff`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
