9dc81c02b4e86b0ecda455a44be1be14f5631c64
siemasiema <siemasiema@siema.siema>
2022-04-05 13:57:41 +0200

    added appointments view

---
+++ viewAppointments.php
@@ -0,0 +1,19 @@
+<h1>Umówione wizyty:</h1>
+<?php 
+$db = new mysqli("localhost", "root", "", "mgmed");
+//zakładamy, że dostaliśmy z requesta
+$patientId = $_REQUEST['id'];
+$q = $db->prepare("SELECT appointment.date, staff.firstName, staff.lastName FROM patientappointment 
+                    LEFT JOIN appointment ON patientappointment.appointment_id = appointment.id
+                    LEFT JOIN staff ON appointment.staff_id = staff.id
+                    WHERE patient_id = ?)");
+$q->bind_param("i", $patientId);
+$q->execute();
+$appointments = $q->get_result();
+while($appointment = $appointments->fetch_assoc()) {
+    $id = $appointment['id'];
+    $staff = $appointment['staff_id'];
+    $date = $appointment['date'];
+    echo "dr. $staffFirstName $staffLastName $date<br>";
+}
+?>
